b4abbc64b67eae859fee6a9f3711cf1fbb56e80e
edbert pagsuyuin <bertpagsuyuin@gmail.com>
2023-11-02 22:29:45 +0800

    done

---
+++ 2Y_OOP_2324_ADeckOfCards/CardGame21.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _2Y_OOP_2324_ADeckOfCards
+{
+    internal class CardGame21
+    {
+        private List<Card> hand = new List<Card>();
+        private int handvalue = 0;
+        private bool isStand = false;
+        private int SearchAce = 0;
+
+        public void SetHand(List<Card> cards)
+        {
+            hand = cards;
+        }
+
+        public int GetValuetoMain()
+        {
+            return handvalue;
+        }
+        public void AddCard(Card card)
+        {
+            hand.Add(card);
+        }
+
+        public void DisplayHand(bool isplayer, bool isdealer, bool isshowcards)
+        {
+            if (isplayer)
+                Console.WriteLine("Player's Hand");
+            else if (isdealer)
+                Console.WriteLine("Dealer's Hand");
+            else
+                Console.WriteLine("Computer's Hand");
+
+            for (int i = 0; i < hand.Count; i++)
+            {
+                if (i == 0 || isshowcards)
+                {
+                    if (hand[i].GetCardFace() == "Ace" && !isshowcards)
+                        Console.WriteLine($"The Card is the {hand[i].GetCardFace()} of {hand[i].GetCardSuit()} with a value of 1");
+                    else
+                        Console.WriteLine($"The Card is the {hand[i].GetCardFace()} of {hand[i].GetCardSuit()} with a value of {hand[i].GetCardValue()}");
+                }
+                else
+                    Console.WriteLine($"The Card is the ???? of ???? with a value of ????");
+            }
+
+            handvalue = GetValue();
+
+            if (isshowcards)
+            {
+                if (isplayer)
+                    Console.WriteLine($"Player's Hand Value is {handvalue}");
+                else if (isdealer)
+                    Console.WriteLine($"Dealer's Hand Value is {handvalue}");
+                else
+                    Console.WriteLine($"Computer's Hand Value is {handvalue}");
+            }
+        }
+
+        public bool Stand(bool isplayer)
+        {
+            if (handvalue == 21)
+            {
+                if (isplayer)
+                    Console.WriteLine("Player BlackJack");
+
+                isStand = true;
+                return true;
+            }
+
+            if (handvalue > 21)
+            {
+                if (isplayer)
+                    Console.WriteLine("Player bust");
+
+                isStand = true;
+                return true;
+            }
+
+            if (isStand)
+                return true;
+
+            return false;
+        }
+        public bool Hit(bool isplayer, bool isdealer)
+        {
+            if (isplayer)
+            {
+                bool ismove = false;
+                do
+                {
+                    if (handvalue == 21)
+                        return false;
+
+                    Console.WriteLine("\nDo you want to draw a card (Hit) or end the turn without drawing a card (Stand)?\n[A] - Hit\n[B] - Stand");
+                    string uInt = Console.ReadLine().ToUpper();
+
+                    switch (uInt)
+                    {
+                        case "A":
+                            Console.WriteLine("Player Hits");
+                            ismove = true;
+                            return true;
+                        case "B":
+                            Console.WriteLine("PLayer Stands");
+                            ismove = true;
+                            isStand = true;
+                            return false;
+                        default:
+                            Console.WriteLine("\nInvalid Input.\nPress any key to continue.");
+                            Console.ReadKey();
+                            break;
+                    }
+                } while (!ismove);
+            }
+
+            if (isdealer)
+            {
+                Console.Write("Dealer is thinking... ");
+                Thread.Sleep(1200);
+                if (handvalue < 17)
+                {
+                    Console.WriteLine("Dealer Hits");
+                    return true;
+                }
+
+                Console.WriteLine("Dealer Stands");
+                isStand = true;
+
+                return false;
+            }
+            
+            Console.Write("Computer is thinking... ");
+            Thread.Sleep(1200);
+            if (handvalue < 17)
+            {
+                Console.WriteLine("Computer Hits");
+                return true;
+            }
+
+            Console.WriteLine("Computer Stands");
+            isStand = true;
+            return false;
+
+        }
+
+        private int GetValue()
+        {
+            int value = 0;
+            for (int i = 0; i < hand.Count; i++)
+            {
+                value += hand[i].GetCardValue();
+            }
+
+            return value;
+        }
+
+        public void Ace(bool isplayer)
+        {
+            for (int i = SearchAce; i < hand.Count; i++)
+            {
+                if (hand[i].GetCardValue() == 1)
+                {
+                    if (isplayer)
+                    {
+                        SetAceValue(hand[i]);
+                    }
+                    else
+                    {
+                        int tempval = GetValue() + 10;
+                        if (tempval >= 17 && tempval <= 21)
+                        {
+                            hand[i].SetNewValue(11);
+                        }
+                    }
+                }
+            }
+            SearchAce = hand.Count;
+        }
+
+        private void SetAceValue(Card card)
+        {
+            bool isvalid = false;
+            int num = 0;
+            do
+            {
+                Console.Write("\nWhat value do you want ace to be? 1 o 11: ");
+                isvalid = int.TryParse(Console.ReadLine(), out num);
+                if (num == 1 || num == 11)
+                {
+                    isvalid = true;
+                    card.SetNewValue(num);
+                    handvalue = handvalue - 1 + num;
+                    Console.WriteLine($"Player's New Hand Value = {handvalue}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input\nPress any key to continue\n");
+                    Console.ReadKey();
+                    isvalid = false;
+                }
+
+            } while (!isvalid);
+        }
+
+    }
+}
--- 2Y_OOP_2324_ADeckOfCards/Program.cs
+++ 2Y_OOP_2324_ADeckOfCards/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Media;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _2Y_OOP_2324_ADeckOfCards
@@ -12,129 +14,189 @@ namespace _2Y_OOP_2324_ADeckOfCards
     {
         static void Main(string[] args)
         {
-            DeckOfCards doc = new DeckOfCards(true);
-
-            //Card draw = doc.drawACard();
-            //Console.WriteLine("Drawing a card");
-            //Console.WriteLine($"The Card is the {draw.GetCardFace()} of {draw.GetCardSuit()} with a value of {draw.GetCardValue()}");
-            //doc.DisplayDeck();
-
-            //Console.WriteLine("\n\nDrawing a couple of cards");
-            //List<Card> hand = new List<Card>();
-            //hand = doc.drawACard(13);
-            //foreach (Card c in hand)
-            //{
-            //    Console.WriteLine($"The Card is the {c.GetCardFace()} of {c.GetCardSuit()} with a value of {c.GetCardValue()}");
-            //}
-
-            PlayerHand player = new PlayerHand();
-            PlayerHand computer = new PlayerHand();
-
-
-            
-            PlayerHand.dealerhand = doc.drawACard(2);
-            computer.SetPlayerHand(doc.drawACard(2));
-            player.SetPlayerHand(doc.drawACard(2));
-            int showcard = 1;
-
-            PlayerHand.DealerHand(showcard);
-            if (PlayerHand.dealervalue < 17)
-            {
-                Console.WriteLine("Dealer Hit");
-                showcard++;
-                PlayerHand.dealerhand.Add(doc.drawACard());
-            }
-            else
-            {
-                Console.WriteLine("Computer Stand");
-            }
-            
-            Console.WriteLine();
-            computer.DisplayPlayerHand(true);
-            int val = computer.GetPlayerValue();
-            if (val < 17)
-            {
-                Console.WriteLine("Computer Hit");
-                computer.AddCard(doc.drawACard());
-            }
-            else
-            {
-                Console.WriteLine("Computer Stand");
-            }
-            Console.WriteLine();
-            player.isAce();
-            player.DisplayPlayerHand(false);
-            if (player.playernextmove())
-            {
-                player.AddCard(doc.drawACard());
-            }
-            Console.WriteLine();
-
-            PlayerHand.DealerHand(showcard+1);
-            Console.WriteLine();
-            computer.DisplayPlayerHand(false);
-            Console.WriteLine();
-            player.DisplayPlayerHand(false);
-            Console.WriteLine();
-
-            Console.ReadKey();
-
-
-            _21CardGame _21CG = new _21CardGame();
-            List<Card> Dhand = doc.drawACard(2);
-            List<Card> Phand = doc.drawACard(2);
-            List<Card> Chand = doc.drawACard(2);
-           
-            int reveal = 1;
+            GameInstructions();
             while (true)
             {
+                CardGame21 player1 = new CardGame21();
+                CardGame21 dealer = new CardGame21();
+                CardGame21 computer1 = new CardGame21();
+
+                Console.Write("\nShuffling the deck...");
+                DeckOfCards doc = new DeckOfCards(true);
+                Thread.Sleep(1000);
+                Console.WriteLine("Done");
+
+                // initialize of hands
+                Console.Write("Distributing cards...");
+                dealer.SetHand(doc.drawACard(2));
+                computer1.SetHand(doc.drawACard(2));
+                player1.SetHand(doc.drawACard(2));
+                Thread.Sleep(1000);
+                Console.WriteLine("Done");
+
+                Thread.Sleep(1000);
                 Console.Clear();
-                int value = 0;
-                value = _21CG.GetAceValue(Dhand);
-                _21CG.DisplayHand(Dhand, reveal);
-                if (value < 17)
-                { 
-                    Dhand.Add(doc.drawACard());
-                    Console.WriteLine("Dealer hit");
-                }
-                else
-                {
-                    if (value < 21)
-                        Console.WriteLine("Dealer stand");
-                    else
-                        Console.WriteLine("Dealer is busted");
-                }
-                Console.WriteLine();
-                value = _21CG.GetAceValue(Chand);
-                _21CG.DisplayHand(Chand, Chand.Count);
-                if (value < 17)
-                {
-                    Chand.Add(doc.drawACard());
-                    Console.WriteLine("Computer hit");
-                }
-                else
-                {
-                    if (value == 21)
-                        Console.WriteLine("Computer won against dealer");
-                    else if (value < 21)
-                        Console.WriteLine("Computer stand");
-                    else
-                        Console.WriteLine("computer is busted");
-                }
-                Console.WriteLine();
-                _21CG.DisplayHand(Phand, Phand.Count);
 
-                //_21CG.DisplayHand(Phand);
-                //_21CG.DisplayHand(Chand);
-                //_21CG.DisplayHand(Dhand, 2);
-                //_21CG.DisplayHand(Phand);
-                //_21CG.DisplayHand(Chand);
-                reveal++;
+                bool isdstand = false;
+                bool isc1stand = false;
+                bool isp1stand = false;
 
+                while (!isc1stand || !isp1stand || !isdstand)
+                {
+                    Console.Clear();
+                    dealer.Ace(false);
+                    dealer.DisplayHand(false, true, false);
+                    if (!dealer.Stand(false))
+                    {
+                        if (dealer.Hit(false, true))
+                            dealer.AddCard(doc.drawACard());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dealer Stands");
+                        isdstand = true;
+                    }
+                    Console.WriteLine();
+
+                    computer1.Ace(false);
+                    computer1.DisplayHand(false, false, false);
+                    if (!computer1.Stand(false))
+                    {
+                        if (computer1.Hit(false, false))
+                            computer1.AddCard(doc.drawACard());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Computer Stands");
+                        isc1stand = true;
+                    }
+                    Console.WriteLine();
+
+                    player1.DisplayHand(true, false, true);
+                    player1.Ace(true);
+                    if (!player1.Stand(true))
+                    {
+                        if (player1.Hit(true, false))
+                            player1.AddCard(doc.drawACard());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player Stands");
+                        isp1stand = true;
+                    }
+
+                    Thread.Sleep(1200);
+                }
+
+                Console.WriteLine("\nPress any key to reveal the hands");
                 Console.ReadKey();
-               
+                Console.Clear();
+                dealer.DisplayHand(false, true, true);
+                Console.WriteLine();
+                computer1.DisplayHand(false, false, true);
+                Console.WriteLine();
+                player1.DisplayHand(true, false, true);
+
+                int dval = dealer.GetValuetoMain();
+                int c1val = computer1.GetValuetoMain();
+                int p1val = player1.GetValuetoMain();
+
+                Console.WriteLine("\n" + Winner(dval, c1val, p1val));
+
+                if (!isAgain())
+                    break;
             }
+
+            Console.ReadKey();
+        } 
+
+        static void GameInstructions()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("WELCOME TO 21 CARD GAME");
+            Console.ResetColor();
+            Console.WriteLine("\nInstructions");
+            Console.WriteLine("Each Player and the Dealer must have atleast 2 cards on their hand.");
+            Console.WriteLine("After getting your cards, you have two options of move, Hit or Stand. Hit - Will draw 1 card from the deck. Stand - Will end your turn.");
+            Console.WriteLine("If if you have an Ace card, you can set its value to 1 or 11");
+            Console.WriteLine("To win, you must have a Hand Value equivalent to 21 or you're the closest to 21.");
+            Console.WriteLine("If your Hand Value is over 21, you are bust meaning you lost.");
+            Console.WriteLine("\nPress any key to start the game.");
+            Console.ReadKey();
         }
 
-    }
+        static string Winner(int dval, int c1val, int p1val)
+        {
+            if (dval > 21 && c1val > 21 && p1val > 21)
+                return "No Winner, Player, Dealer, and Computer are Bust.";
+            else if (dval > 21 && c1val > 21)
+                return "Dealer and Computer Bust, Player Win";
+            else if (p1val > 21 && c1val > 21)
+                return "Player and Computer Bust, Dealer Win";
+            else if (dval > 21 && p1val > 21)
+                return "Player and Dealer Bust, Computer Win";
+            else if (p1val == 21 && (c1val < 21 && dval < 21))
+                return "Player Win";
+            else if (c1val == 21 && (p1val < 21 && dval < 21))
+                return "Computer Win";
+            else if (dval == 21 && (p1val < 21 && c1val < 21))
+                return "Dealer Win";
+            else if (dval == c1val && dval == p1val)
+                return "No winner, Player, Dealer, and Computer are Draw.";
+            else if ((dval < p1val && c1val < p1val) && (p1val < 21 && dval < 21 && c1val < 21))
+                return "Player Win";
+            //else if ((c1val < p1val) && (p1val < 21 && c1val < 21 && p1val != dval))
+            //    return "Player Win";
+            else if ((p1val < c1val && dval < c1val) && (c1val < 21 && p1val < 21 && dval < 21))
+                return "Computer Win";
+            //else if ((dval < c1val) && (c1val < 21 && dval < 21 && c1val != p1val))
+            //    return "Computer Win";
+            else if ((c1val < dval && p1val < dval) && (dval < 21 && c1val < 21 && p1val < 21))
+                return "Dealer Win";
+            //else if ((p1val < dval) && (dval < 21 && p1val < 21 && dval != c1val))
+            //    return "Dealer Win";
+            else if (dval == c1val)
+                return "Dealer and Computer are Draw";
+            else if (dval == p1val)
+                return "Player and Dealer are Draw";
+            else if (c1val == p1val)
+                return "Player and Computer are Draw";
+            else if (p1val == 21)
+                return "Player Win";
+            else if (c1val == 21)
+                return "Computer Win";
+            else if (dval == 21)
+                return "Dealer Win";
+           
+
+            return " ";
+        }
+
+        static bool isAgain()
+        {
+            bool ismove = false;
+            do
+            {
+                Console.WriteLine("\nDo you want to play again?\n[A] - Yes\n[B] - No");
+                string uInt = Console.ReadLine().ToUpper();
+
+                switch (uInt)
+                {
+                    case "A":
+                        ismove = true;
+                        return true;
+                    case "B":
+                        ismove = true;
+                        return false;
+                    default:
+                        Console.WriteLine("\nInvalid Input.\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!ismove);
+
+            return false;
+        }
+
+   }
 }
